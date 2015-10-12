be08b4fe99403d23c0e6a0d4b4ae4e9b34657ef7
tokjos <tokjos@statoil.com>
2015-10-12 13:28:16 +0200

    Added store

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Torill' });
+});
+
+router.get('/store', function (req, res) {
+  //res.send('Store is handled');
+	res.render('index', { title: 'Store' });
 });
 
 module.exports = router;
