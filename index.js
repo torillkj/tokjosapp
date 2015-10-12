5973c19e68ad9732c5b8fee9a877bedc4a4d53cd
tokjos <tokjos@statoil.com>
2015-10-12 10:39:46 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by TOKJOS on 12.10.2015.
+ */
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+})
+
+app.listen(3000); //Lytter til rotnoden
