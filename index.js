ac5008e10eec7f0cba3a02312a31a89bee996132
tokjos <tokjos@statoil.com>
2015-10-12 12:39:01 +0200

    Changed port allocation

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
  */
 var express = require('express');
 var app = express();
+var port = (process.env.PORT || '3000');
 
 app.get('/', function(req, res){
   res.send('Hello World!!');
 })
 
-app.listen(3000); //Lytter til rotnoden
+app.listen(port); //Lytter til rotnoden
 
-console.log('Server started at port 3000');
+console.log('Server started at port 3000' + port);
