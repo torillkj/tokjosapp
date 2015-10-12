ffc7710aac239bfd8357d462b3fded176b75c82f
tokjos <tokjos@statoil.com>
2015-10-12 11:16:17 +0200

    Initial commit

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('Hello World!!');
 })
 
 app.listen(3000); //Lytter til rotnoden
+
+console.log('Server started at port 3000');
